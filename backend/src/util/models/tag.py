7c9cc2e97a1bae6ee953042a5a9440cd8ccec32b
Olena <155203553+elgatoafk@users.noreply.github.com>
2024-07-19 22:31:50 +0300

    Add tag model

---
+++ backend/src/util/models/tag.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Column, Integer, String
+from util.db import Base
+
+class Tag(Base):
+    __tablename__ = "tags"
+    tag_id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    tag_name = Column(String, unique=True, nullable=False)
+    
+
