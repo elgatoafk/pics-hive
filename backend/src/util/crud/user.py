ed28a3585e6098269bb8d050e9569da4bf44f051
MarsIana1804 <139183555+MarsIana1804@users.noreply.github.com>
2024-07-22 23:40:51 +0200

    user.py files initial versions

---
+++ backend/src/util/crud/user.py
@@ -0,0 +1,39 @@
+import bcrypt
+from sqlalchemy.orm import Session
+from backend.src.util.schemas import user
+from backend.src.util.models import models
+
+dbg = True
+
+def get_user(db: Session, user_id: int):
+    if dbg: print('get_user')
+    return db.query(models.User).filter(models.User.id == user_id).first()
+
+def get_user_by_email(db: Session, email: str):
+    if dbg: print('get_user_by_email')
+    return db.query(models.User).filter(models.User.email == email).first()
+
+def get_users(db: Session, skip: int = 0, limit: int = 100):
+    if dbg: print('get_users')
+    return db.query(models.User).offset(skip).limit(limit).all()
+
+def hash_password(password: str) -> str:
+    if dbg: print('hash_password')
+    # Generate a salt and hash the password
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password.decode('utf-8')
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    if dbg: print('verify_password')
+    # Verify the given password against the stored hashed password
+    return bcrypt.checkpw(plain_password.encode('utf-8'), hashed_password.encode('utf-8'))
+
+def create_user(db: Session, user: user.UserCreate):
+    if dbg: print('create_user')
+    hashed_password = hash_password(user.password)
+    db_user = models.User(email=user.email, hashed_password=hashed_password, role=user.role)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
