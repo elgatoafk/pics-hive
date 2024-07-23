d4b12907411d92f25a5b61032aebfdd9daaffa81
Olena <155203553+elgatoafk@users.noreply.github.com>
2024-07-23 23:06:51 +0300

    Remove redundant routes for tags

--- backend/src/routes/tag.py
+++ backend/src/routes/tag.py
@@ -13,19 +13,3 @@ async def create_tag_route(tag: TagCreate, db: Session = Depends(get_db)):
 @router.get("/tags/", response_model=TagResponse)
 async def get_tag_route(tag_name: str, db: Session = Depends(get_db)):
     return await get_tag(db, tag_name)
-
-@router.put("/tags/", response_model=TagResponse)
-async def update_tag_route(tag: TagCreate, tag_id: int, db: Session = Depends(get_db)):
-    tag_updated = await update_tag(tag, tag_id, db)
-    if not tag_updated:
-        raise HTTPException(status_code=404, detail="Tag not found")
-    return tag_updated
-
-@router.delete("/tags/", response_model=TagResponse)
-async def delete_tag_route(tag_name: str, db: Session = Depends(get_db)):
-    tag_db = await get_tag(tag_name, db)
-    if not tag_db:
-        raise HTTPException(status_code=404, detail="Tag not found")
-    else:
-        await delete_tag(tag_db, db)
-        return {"detail": "Tag deleted successfully"}
