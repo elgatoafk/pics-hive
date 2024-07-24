f61c724b9232d35553eee04baceea422235c9577
MarsIana1804 <139183555+MarsIana1804@users.noreply.github.com>
2024-07-24 22:32:44 +0200

    config/ files

--- backend/src/config/config.py
+++ backend/src/config/config.py
@@ -1,21 +1,34 @@
 import os
-from pydantic import BaseSettings
-from dotenv import load_dotenv, dotenv_values
+#from pydantic import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
-#@Everyone, create .env file in this folder
-# with the required fields (SECRET_KEY, DATABASE_URL),
-# each one who works on DB should set up Postgres in Docker
-
-
-load_dotenv()
 
 class Settings(BaseSettings):
+    
     PROJECT_NAME: str = "PhotoShare"
-    SECRET_KEY: str = os.getenv("SECRET_KEY", "your_secret_key")
+    SECRET_KEY: str = os.getenv("SECRET_KEY", "my_secret_key")
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
-    DATABASE_URL: str = os.getenv("DATABASE_URL", "postgresql+psycopg2://user:password@localhost/dbname")
+
+    DATABASE_URL: str = os.getenv("DATABASE_URL", "postgresql://user:password@localhost/dbname")
+    DATABASE_USER: str
+    DATABASE_PASSWORD: str
+    DATABASE_DB_NAME: str
+    DATABASE_DOMAIN: str
+    DATABASE_PORT: int
+    
+    CLOUDINARY_CLOUD_NAME: str
+    CLOUDINARY_API_KEY: str
+    CLOUDINARY_API_SECRET: str
+    CLOUDINARY_API_URL: str
+
+    model_config = SettingsConfigDict(env_file=os.path.join(os.path.dirname(__file__), '.env'))
+
 
 settings = Settings()
+
+#print("Loaded settings:", settings.model_dump())  # This will print all settings
+
+
 
 
