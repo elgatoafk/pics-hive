6ec814ff49040f364d772224e56b9d37d62278ae
MARVRK <155836501+MARVRK@users.noreply.github.com>
2024-07-26 18:43:38 +0200

    updated async function for CRUD

--- backend/src/util/crud/tag.py
+++ backend/src/util/crud/tag.py
@@ -1,32 +1,32 @@
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi import Depends
 from backend.src.util.db import get_db
 from backend.src.util.models.tag import Tag
 from backend.src.util.schemas.tag import TagCreate
 
 
-def get_tag(tag_name: str, db:Session = Depends(get_db)) -> Tag:
-    return db.query(Tag).filter(Tag.name == tag_name).first()
+async def get_tag(tag_name: str, db:AsyncSession = Depends(get_db)) -> Tag:
+     return await db.execute(db.query(Tag).filter(Tag.name == tag_name).first())
 
-def create_tag(body: TagCreate, db:Session = Depends(get_db)) -> Tag:
+async def create_tag(body: TagCreate, db:AsyncSession = Depends(get_db)) -> Tag:
     tag_db = Tag(name=body.name)
     db.add(tag_db)
-    db.commit()
-    db.refresh(tag_db)
+    await db.commit()
+    await db.refresh(tag_db)
     return tag_db
-def update_tag(body: TagCreate, tag_id: int, db:Session = Depends(get_db)) -> Tag:
-    tag_db = db.query(Tag).filter(Tag.id == tag_id).first()
+async def update_tag(body: TagCreate, tag_id: int, db:AsyncSession = Depends(get_db)) -> Tag:
+    tag_db = db.execute(db.query(Tag).filter(Tag.id == tag_id).first())
     if tag_db:
         tag_db.name = body.name
-        db.commit()
-        db.refresh(tag_db)
+        await db.commit()
+        await db.refresh(tag_db)
         return tag_db
-def delete_tag(tag_name: str, db:Session = Depends(get_db)) -> Tag:
-    tag_db = db.query(Tag).filter(Tag.name == tag_name).first()
+async def delete_tag(tag_name: str, db:AsyncSession = Depends(get_db)) -> Tag:
+    tag_db = db.execute(db.query(Tag).filter(Tag.name == tag_name).first())
     if tag_db:
-        db.delete(tag_db)
-        db.commit()
-        db.refresh(tag_db)
+        await db.delete(tag_db)
+        await db.commit()
+        await db.refresh(tag_db)
         return tag_db
 
 
