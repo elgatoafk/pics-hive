1d333a836c08268bbdad1bb43a3c588d727c2b25
MARVRK <155836501+MARVRK@users.noreply.github.com>
2024-07-27 20:13:12 +0200

    adding dockstrings

--- backend/src/routes/photo.py
+++ backend/src/routes/photo.py
@@ -9,14 +9,46 @@ router = APIRouter()
 
 @router.post("/photos/", response_model=PhotoResponse)
 async def creat_photo_route(photo: PhotoCreate, user_id: int, db: Session = Depends(get_db)):
+    """
+    Creates a new photo in the database.
+
+    Parameters:
+    photo (PhotoCreate): The photo data to be created.
+    user_id (int): The ID of the user creating the photo.
+    db (Session, optional): The database session. Defaults to Depends(get_db).
+
+    Returns:
+    PhotoResponse: The created photo data.
+    """
     return await create_photo(db, photo, user_id)
 
 @router.get("/photos/", response_model=PhotoResponse)
 async def get_photo_route(photo_id: int, db: Session = Depends(get_db)):
+    """
+    Retrieves a photo from the database based on the provided photo ID.
+
+    Parameters:
+    photo_id (int): The unique identifier of the photo to retrieve.
+    db (Session, optional): The database session. Defaults to Depends(get_db).
+
+    Returns:
+    PhotoResponse: The retrieved photo data. If the photo is not found, returns None.
+    """
     return await get_photo(db, photo_id)
 
 @router.put("/photos/", response_model=PhotoResponse)
 async def update_photo_route(photo: PhotoUpdate, photo_id: int, db: Session = Depends(get_db)):
+    """
+    Updates an existing photo in the database based on the provided photo ID.
+
+    Parameters:
+    photo (PhotoUpdate): The updated photo data.
+    photo_id (int): The unique identifier of the photo to update.
+    db (Session, optional): The database session. Defaults to Depends(get_db).
+
+    Returns:
+    PhotoResponse: The updated photo data. If the photo is not found, raises a 404 HTTPException.
+    """
     updated_photo = await update_photo(db, photo, photo_id)
     if not updated_photo:
         raise HTTPException(status_code=404, detail="Photo not found")
@@ -24,6 +56,16 @@ async def update_photo_route(photo: PhotoUpdate, photo_id: int, db: Session = De
 
 @router.delete("/photos/")
 async def delete_photo_route(photo_id: int, db: Session = Depends(get_db)):
+    """
+    Deletes a photo from the database based on the provided photo ID.
+
+    Parameters:
+    photo_id (int): The unique identifier of the photo to delete.
+    db (Session, optional): The database session. Defaults to Depends(get_db).
+
+    Returns:
+    dict: A dictionary with a 'detail' key indicating the success message. If the photo is not found, raises a 404 HTTPException.
+    """
     photo_db = await get_photo(db, photo_id)
     if not photo_db:
         raise HTTPException(status_code=404, detail="Photo not found")
@@ -38,6 +80,21 @@ async def generate_qr_code(
 ):
     """Generates a QR code for the given image URL.
 
+    Args:
+        photo_url (str): The URL of the image for which the QR code needs to be generated.
+        request (Request): The FastAPI request object.
+        user (User, optional): The authenticated user. Defaults to Depends(get_current_user()).
+
+    Returns:
+        Response: A FastAPI response object containing the generated QR code as a PNG image.
+        The response has the following properties:
+            - content: The QR code image data.
+            - media_type: The MIME type of the content, which is "image/png".
+            - headers: A dictionary containing the "content-disposition" header with the value "inline".
+            - status_code: The HTTP status code, which is 200.
+
+    Raises:
+        HTTPException: If the user is not authenticated.
     """
     if user:
         qr_code = await PhotoService.generate_qr_code(photo_url=photo_url)
@@ -47,3 +104,5 @@ async def generate_qr_code(
             headers={"content-disposition": "inline"},
             status_code=200,
         )
+    else:
+        raise HTTPException(status_code=401, detail="User not authenticated")
--- backend/src/routes/tag.py
+++ backend/src/routes/tag.py
@@ -2,18 +2,36 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 from sqlalchemy.orm import Session
-
+from backend.src.util.schemas.tag import PhotoTaggingResponse,PhotoTaggingRequest
 from backend.src.util import models, schemas, db
-from sqlalchemy import and_
-from typing import List
+from backend.src.util.db import get_db
+from backend.src.util.models.tag import Tag
+from backend.src.util.crud.tag import get_tag_by_name
+
 
 router = APIRouter()
 
 
-@router.post("/photos/{photo_id}/tags", response_model=schemas.PhotoTaggingResponse)
+@router.post("/photos/{photo_id}/tags", response_model=PhotoTaggingResponse)
 async def add_tags_to_photo(
-        photo_id: int, tagging_request: schemas.PhotoTaggingRequest, db: AsyncSession = Depends(db.get_db)
+        photo_id: int, tagging_request: PhotoTaggingRequest, db: AsyncSession = Depends(db.get_db)
 ):
+    """
+    Adds tags to a photo in the database.
+
+    This function retrieves a photo from the database based on the provided photo ID,
+    checks if the photo exists, and then adds tags to the photo. If the photo does not exist,
+    a 404 Not Found error is raised. If the number of tags to be added exceeds 5, a 400 Bad Request
+    error is raised.
+
+    Parameters:
+    photo_id (int): The ID of the photo to which tags will be added.
+    tagging_request (schemas.PhotoTaggingRequest): The request containing the tags to be added.
+    db (AsyncSession): The database session to use for querying and updating. This parameter is optional and defaults to the session provided by the `get_db` dependency.
+
+    Returns:
+    schemas.PhotoTaggingResponse: A response containing the ID of the photo and the list of tags added.
+    """
     # Fetch the photo from the database
     result = await db.execute(select(models.Photo).filter_by(id=photo_id))
     photo = result.scalars().first()
@@ -48,8 +66,20 @@ async def add_tags_to_photo(
         tags=[tag.name for tag in photo.tags]
     )
 
-
-@router.get("/tags/", response_model=TagResponse)
+@router.get("/tags/", response_model=PhotoTaggingResponse)
 async def get_tag_route(tag_name: str, db: Session = Depends(get_db)):
-    return await get_tag(db, tag_name)
+    """
+    Retrieve a tag by its name.
+
+    This function retrieves a tag from the database based on the provided tag name.
+    If the tag is found, it is returned. If the tag is not found, a 404 Not Found error is raised.
+
+    Parameters:
+    tag_name (str): The name of the tag to retrieve.
+    db (Session): The database session to use for querying. This parameter is optional and defaults to the session provided by the `get_db` dependency.
+
+    Returns:
+    Tag: The retrieved tag if found. If the tag is not found, a 404 Not Found error is raised.
+    """
+    return await get_tag_by_name(db, tag_name)
 
