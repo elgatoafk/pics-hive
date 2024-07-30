b8d705f9f0d359d1346df668e6395f9dc237b9e4
MARVRK <155836501+MARVRK@users.noreply.github.com>
2024-07-30 23:36:07 +0200

    fixing bugs in Photos model

--- backend/src/routes/photo.py
+++ backend/src/routes/photo.py
@@ -1,31 +1,73 @@
 import asyncio
 
 from fastapi import APIRouter, Depends, HTTPException, status
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import joinedload
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+import io
+
 from backend.src.util.schemas.photo import PhotoCreate, PhotoUpdate, PhotoResponse
-from backend.src.util.crud.photo import create_photo, get_photo, update_photo, delete_photo
-from backend.src.util.db import get_db
+from backend.src.util.crud.photo import get_photo, update_photo, delete_photo
+from backend.src.util.models import Photo
 from backend.src.services.photos import PhotoService
 from starlette.responses import StreamingResponse
+from backend.src.util.db import get_db
+
+from backend.src.util.models import Tag
+
 router = APIRouter()
 
-@router.post("/photos/", response_model=PhotoResponse)
-async def creat_photo_route(photo: PhotoCreate, user_id: int, db: Session = Depends(get_db)):
+@router.post ("/photos/", response_model=PhotoResponse)
+async def create_photo_route (body: PhotoCreate, user_id: int, db: AsyncSession = Depends (get_db)):
     """
     Creates a new photo in the database.
 
     Parameters:
-    photo (PhotoCreate): The photo data to be created.
+    body (PhotoCreate): The data for the new photo, including description, URL, and tags.
     user_id (int): The ID of the user creating the photo.
-    db (Session, optional): The database session. Defaults to Depends(get_db).
+    db (AsyncSession, optional): The database session. Defaults to Depends(get_db).
 
     Returns:
-    PhotoResponse: The created photo data.
-    """
-    return await create_photo(db, photo, user_id)
+    PhotoResponse: The created photo data, including its ID, user ID, URL, description, and tags.
 
-@router.get("/photos/", response_model=PhotoResponse)
-async def get_photo_route(photo_id: int, db: Session = Depends(get_db)):
+    Raises:
+    HTTPException: If an internal server error occurs during the creation process.
+    """
+    try:
+        db_photo = Photo (description=body.description, url=body.url, user_id=user_id)
+
+        if body.tags:
+            tags_list = []
+            for tag_name in body.tags:
+
+                result = await db.execute (select (Tag).filter (Tag.tag_name == tag_name))
+                tag = result.scalars().first ()
+                if not tag:
+                    tag = Tag (tag_name=tag_name)
+                    db.add (tag)
+                    await db.flush()
+                tags_list.append (tag)
+            db_photo.tags = tags_list
+
+        db.add (db_photo)
+        await db.commit ()
+        await db.refresh (db_photo)
+
+
+        stmt = select (Photo).options(joinedload(Photo.tags)).filter_by(id=db_photo.id)
+        result = await db.execute (stmt)
+        db_photo = result.scalars().first()
+
+        response = PhotoResponse (id=db_photo.id, user_id=db_photo.user_id, url=db_photo.url, description=db_photo.description,
+            tags=[tag.tag_name for tag in db_photo.tags])
+
+        return response
+
+    except Exception as e:
+        raise HTTPException (status_code=500, detail="Internal Server Error")
+
+@router.get("/photos/{photo_id}", response_model=PhotoResponse)
+async def get_photo_route(photo_id: int, db: AsyncSession = Depends(get_db)):
     """
     Retrieves a photo from the database based on the provided photo ID.
 
@@ -38,26 +80,13 @@ async def get_photo_route(photo_id: int, db: Session = Depends(get_db)):
     """
     return await get_photo(db, photo_id)
 
-@router.put("/photos/", response_model=PhotoResponse)
-async def update_photo_route(photo: PhotoUpdate, photo_id: int, db: Session = Depends(get_db)):
-    """
-    Updates an existing photo in the database based on the provided photo ID.
-
-    Parameters:
-    photo (PhotoUpdate): The updated photo data.
-    photo_id (int): The unique identifier of the photo to update.
-    db (Session, optional): The database session. Defaults to Depends(get_db).
-
-    Returns:
-    PhotoResponse: The updated photo data. If the photo is not found, raises a 404 HTTPException.
-    """
-    updated_photo = await update_photo(db, photo, photo_id)
-    if not updated_photo:
-        raise HTTPException(status_code=404, detail="Photo not found")
+@router.put("/photos/{photo_id}", response_model=PhotoResponse)
+async def update_photo_route(body: PhotoUpdate, photo_id: int, db: AsyncSession = Depends(get_db)):
+    updated_photo = await update_photo(db, body, photo_id)
     return updated_photo
 
 @router.delete("/photos/")
-async def delete_photo_route(photo_id: int, db: Session = Depends(get_db)):
+async def delete_photo_route(photo_id: int, db: AsyncSession = Depends(get_db)):
     """
     Deletes a photo from the database based on the provided photo ID.
 
@@ -71,28 +100,56 @@ async def delete_photo_route(photo_id: int, db: Session = Depends(get_db)):
     await delete_photo(db, photo_id)
     return {"detail": "Photo deleted successfully"}
 
-
 @router.post("/generate_qrcode/{photo_id}")
-async def generate_qr_code(
-    photo_id: int, db: Session = Depends(get_db)
-):
-    """Generates a QR code for the given photo ID.
+async def generate_qr_code(photo_id: int, db: AsyncSession = Depends(get_db)):
+    """
+    Generates a QR code for a photo based on its ID.
 
-    Args:
-        photo_ID (str): The ID of the image for which the QR code needs to be generated.
-        db (Session, optional): The database session. Defaults to Depends(get_db).
+    Parameters:
+    photo_id (int): The unique identifier of the photo for which to generate the QR code.
+    db (AsyncSession, optional): The database session. Defaults to Depends(get_db).
 
     Returns:
-         image/png: streamingresponse
-
-    Raises:
-        HTTPException: If the photo is not found
+    StreamingResponse: A response object containing the generated QR code image in PNG format.
+    If the photo is not found, raises a 404 HTTPException.
+    If the QR code generation fails, also raises a 404 HTTPException.
     """
-    photo = await get_photo(photo_id)
+    photo = await get_photo(db, photo_id)
     if photo is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not Found")
-    qr_code = await PhotoService.generate_qr_code(photo.photo_url)
+
+    qr_code = await PhotoService.generate_qr_code(photo.url)
+
     if qr_code is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not Found")
-    return StreamingResponse(qr_code, media_type="image/png", status_code=status.HTTP_201_CREATED)
 
+    if isinstance(qr_code, int):
+        qr_code = str(qr_code).encode('utf-8')
+    return StreamingResponse(io.BytesIO(qr_code), media_type="image/png", status_code=status.HTTP_201_CREATED)
+
+@router.post("/generate_qrcode/{photo_id}")
+async def generate_qr_code(photo_id: int, db: AsyncSession = Depends(get_db)):
+    """
+    Generates a QR code for a photo based on its ID.
+
+    Parameters:
+    photo_id (int): The unique identifier of the photo for which to generate the QR code.
+    db (AsyncSession, optional): The database session. Defaults to Depends(get_db).
+
+    Returns:
+    StreamingResponse: A response object containing the generated QR code image in PNG format.
+    If the photo is not found, raises a 404 HTTPException.
+    If the QR code generation fails, also raises a 404 HTTPException.
+    """
+    photo = await get_photo(db, photo_id)
+    if photo is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not Found")
+
+    qr_code = await PhotoService.generate_qr_code(photo.url)
+
+    if qr_code is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not Found")
+
+    if isinstance(qr_code, int):
+        qr_code = str(qr_code).encode('utf-8')
+    return StreamingResponse(io.BytesIO(qr_code), media_type="image/png", status_code=status.HTTP_201_CREATED)
--- backend/src/util/crud/photo.py
+++ backend/src/util/crud/photo.py
@@ -1,64 +1,59 @@
+
+from sqlalchemy.orm import joinedload
 from fastapi import HTTPException, status
-from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.ext.asyncio import AsyncSession
-from backend.src.util.models.photo import Photo, photo_m2m_tag
-from backend.src.util.schemas.photo import PhotoCreate
+from backend.src.util.models.photo import Photo
+from backend.src.util.schemas.photo import PhotoUpdate, PhotoResponse
 from sqlalchemy.future import select
 
-from backend.src.util.logging_config import logger
-from backend.src.util.models import User
 
 async def get_photo(db: AsyncSession, photo_id: int ):
     """
-    Retrieve a photo by its ID from the database.
+    Asynchronously retrieves a photo from the database by its ID.
 
     Parameters:
+    - db (AsyncSession): The SQLAlchemy AsyncSession object for database operations.
     - photo_id (int): The unique identifier of the photo to retrieve.
-    - db (AsyncSession): The asynchronous database session. It is optional and will be injected by the FastAPI framework.
 
     Returns:
-    - Photo: The retrieved photo object if found, otherwise None.
+    - PhotoResponse: A response object containing the photo's details.
+
+    Raises:
+    - HTTPException: If the photo is not found in the database, a 404 Not Found error is raised.
+    - HTTPException: If an error occurs during database operations, a 500 Internal Server Error is raised.
     """
-    result = await db.execute(select(Photo).filter(Photo.id == photo_id))
-    return result.scalars().first()
-
-
-async def create_photo(db: AsyncSession, body: PhotoCreate, user_id: int):
-    """
-    Create a new photo in the database.
-
-    Parameters:
-    - body (PhotoCreate): The photo data to be created. It should be an instance of the PhotoCreate schema.
-    - user_id (int): The unique identifier of the user who is creating the photo.
-    - db (AsyncSession): The asynchronous database session. It is optional and will be injected by the FastAPI framework.
-
-    Returns:
-    - Photo: The newly created photo object.
-    """
-    db_photo = Photo(**body.dict(), user_id=user_id)
-    db.add(db_photo)
-    await db.commit()
-    await db.refresh(db_photo)
-    return db_photo
-
-
-async def update_photo(db: AsyncSession, body: PhotoCreate,user: User, photo_id: int ):
-    """
-    Update an existing photo in the database.
-
-    Parameters:
-    - body (PhotoCreate): The updated photo data. It should be an instance of the PhotoCreate schema.
-    - photo_id (int): The unique identifier of the photo to update.
-    - db (AsyncSession): The asynchronous database session. It is optional and will be injected by the FastAPI framework.
-
-    Returns:
-    - Photo: The updated photo object. If the photo with the given ID does not exist, it returns None.
-    """
-    await db.commit ()
     try:
-        result = await db.execute (select(Photo).filter(Photo.id == photo_id))
-        db_photo = result.scalars ().first ()
+        result = await db.execute(select(Photo).options(joinedload(Photo.tags)).filter(Photo.id == photo_id))
+        db_photo = result.scalars().first()
+        if not db_photo:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Photo not found")
 
+        tags = [tag.tag_name for tag in db_photo.tags]
+        response = PhotoResponse (id=db_photo.id, user_id=db_photo.user_id, url=db_photo.url, description=db_photo.description, tags=tags)
+        return response
+
+    except Exception as e:
+        raise HTTPException (status_code=500, detail=str (e))
+
+async def update_photo (db: AsyncSession, body: PhotoUpdate, photo_id: int) -> Photo:
+    """
+    Asynchronously updates a photo in the database by its ID.
+
+    Parameters:
+    - db (AsyncSession): The SQLAlchemy AsyncSession object for database operations.
+    - body (PhotoUpdate): The updated photo data.
+    - photo_id (int): The unique identifier of the photo to update.
+
+    Returns:
+    - Photo: The updated photo object.
+
+    Raises:
+    - HTTPException: If the photo is not found in the database, a 404 Not Found error is raised.
+    - HTTPException: If an error occurs during database operations, a 500 Internal Server Error is raised.
+    """
+    try:
+        result = await db.execute (select (Photo).options (joinedload (Photo.tags)).filter (Photo.id == photo_id))
+        db_photo = result.scalars ().first ()
         if not db_photo:
             raise HTTPException (status_code=status.HTTP_404_NOT_FOUND, detail="Photo not found")
 
@@ -69,31 +64,33 @@ async def update_photo(db: AsyncSession, body: PhotoCreate,user: User, photo_id:
         await db.refresh (db_photo)
         return db_photo
 
-    except SQLAlchemyError as e:
-        logger.error (f"Error updating photo: {e}")
-        raise HTTPException (status_code=500, detail="Internal Server Error")
+    except Exception as e:
+        await db.rollback ()
+        raise HTTPException (status_code=500, detail=str (e))
 
 async def delete_photo(db: AsyncSession, photo_id: int):
+    """
+    Asynchronously deletes a photo from the database by its ID.
+
+    Parameters:
+    - db (AsyncSession): The SQLAlchemy AsyncSession object for database operations.
+    - photo_id (int): The unique identifier of the photo to delete.
+
+    Returns:
+    - Photo: The deleted photo object.
+
+    Raises:
+    - HTTPException: If the photo is not found in the database, a 404 Not Found error is raised.
+    - HTTPException: If an error occurs during database operations, a 500 Internal Server Error is raised.
+    """
     try:
-        # Deleting tags associated with the photo
-        tags_to_delete = await db.execute(
-            select(photo_m2m_tag).filter(photo_m2m_tag.c.photo_id == photo_id)
-        )
-        for tag in tags_to_delete:
-            await db.delete(tag)
-
-        # Deleting the photo
-        photo_to_delete = await db.execute(
-            select(Photo).filter(Photo.id == photo_id)
-        )
-        photo = photo_to_delete.scalars().first()
-
-        if photo:
-            await db.delete(photo)
-            await db.commit()
-        else:
-            raise HTTPException(status_code=404, detail="Photo not found")
+        result = await db.execute(select(Photo).filter(Photo.id == photo_id))
+        db_photo = result.scalars ().first ()
+        if not db_photo:
+            raise HTTPException (status_code=status.HTTP_404_NOT_FOUND, detail="Photo not found")
+        await db.delete(db_photo)
+        await db.commit()
+        return db_photo
 
     except Exception as e:
-        await db.rollback()
-        raise HTTPException(status_code=500, detail=f"Error deleting photo: {str(e)}")
+        raise HTTPException (status_code=500, detail=str (e))
